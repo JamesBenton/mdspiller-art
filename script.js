da1c39e31db8a13b5a0c71ad6670e5b34a522ed1
JamesBenton <jamesbentonemail@gmail.com>
2025-06-15 15:56:23 -0500

    Added Image descriptions

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function openModal(img) {
   const modal = document.getElementById("imgModal");
   const modalImg = document.getElementById("modalImage");
   const caption = document.getElementById("caption");
+  const description = document.getElementById("description");
 
   modal.style.display = "flex";
   modalImg.src = img.src;
   caption.textContent = img.alt;
+  description.textContent = img.getAttribute('data-description') || "";
 }
 
 function closeModal() {
