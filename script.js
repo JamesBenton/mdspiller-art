5b8120f9ac470333508345127cf6423219a49b4b
JamesBenton <jamesbentonemail@gmail.com>
2025-06-15 15:47:15 -0500

    Fixed js file

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-<script>
-  function openModal(img) {
-    const modal = document.getElementById("imgModal");
-    const modalImg = document.getElementById("modalImage");
-    const caption = document.getElementById("caption");
+function openModal(img) {
+  const modal = document.getElementById("imgModal");
+  const modalImg = document.getElementById("modalImage");
+  const caption = document.getElementById("caption");
 
-    modal.style.display = "flex";
-    modalImg.src = img.src;
-    caption.textContent = img.alt;
-  }
+  modal.style.display = "flex";
+  modalImg.src = img.src;
+  caption.textContent = img.alt;
+}
 
-  function closeModal() {
-    document.getElementById("imgModal").style.display = "none";
-  }
-</script>
+function closeModal() {
+  document.getElementById("imgModal").style.display = "none";
+}
